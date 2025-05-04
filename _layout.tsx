d68f3a9be3c35d3ebff5981520b57980cc33705b
isidxraaaa <163931478+isidxraaaa@users.noreply.github.com>
2025-05-04 19:30:32 -0400

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -1,7 +1,30 @@
-import { Stack } from 'expo-router';
+// Archivo: (tabs)/_layout.tsx
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import AdminMain from '../admin/main';
+
+const Drawer = createDrawerNavigator();
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="AdminMain"
+        screenOptions={{
+          headerShown: false, // Ocultar el encabezado por defecto
+        }}
+      >
+        <Drawer.Screen
+          name="AdminMain"
+          component={AdminMain}
+          options={{
+            drawerLabel: 'Panel Administrativo',
+            drawerIcon: () => <MaterialCommunityIcons name="account-cog" size={24} color="black" />
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
