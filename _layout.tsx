59da1202ae740179d139bb208113d67071e85497
isidxraaaa <163931478+isidxraaaa@users.noreply.github.com>
2025-05-04 21:28:47 -0400

    Update _layout.tsx

--- _layout.tsx
+++ _layout.tsx
@@ -1,30 +1,33 @@
 // Archivo: (tabs)/_layout.tsx
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import AdminMain from '../admin/main';
+import ForgotPassword from './forgotPassword';
 
 const Drawer = createDrawerNavigator();
 
 export default function Layout() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="AdminMain"
-        screenOptions={{
-          headerShown: false, // Ocultar el encabezado por defecto
+    <Drawer.Navigator
+      initialRouteName="AdminMain"
+      screenOptions={{
+        headerShown: false, // Puedes ajustar esto según tus necesidades
+      }}
+    >
+      <Drawer.Screen
+        name="AdminMain"
+        component={AdminMain}
+        options={{
+          title: 'Panel Administrativo',
         }}
-      >
-        <Drawer.Screen
-          name="AdminMain"
-          component={AdminMain}
-          options={{
-            drawerLabel: 'Panel Administrativo',
-            drawerIcon: () => <MaterialCommunityIcons name="account-cog" size={24} color="black" />
-          }}
-        />
-      </Drawer.Navigator>
-    </NavigationContainer>
+      />
+      <Drawer.Screen
+        name="ForgotPassword"
+        component={ForgotPassword}
+        options={{
+          title: 'Recuperar Contraseña',
+        }}
+      />
+    </Drawer.Navigator>
   );
 }
